906f953f6c4a7531b3c2121a5d65af30319a3e55
Raphael Saldanha <rfsaldanha@gmail.com>
2023-10-24 12:52:51 +0200

    parquet creation

--- db creation.R
+++
@@ -1,9 +0,0 @@
-library(nycflights13)
-
-flights$time_hour <- as.character(flights$time_hour)
-
-conn <- DBI::dbConnect(duckdb::duckdb(), "flights.duckdb")
-
-DBI::dbWriteTable(conn, name = "flights", value = flights)
-
-DBI::dbDisconnect(conn, shutdown = TRUE)
